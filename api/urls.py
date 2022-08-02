ef896ababb08c5b859cee0a846a98b5083eedd13
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-08-03 00:22:32 +0600

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', get_status),
-    path('register/', RegistrationView.as_view()),
-    path('login/', obtain_auth_token),
-    path('patient/', PatientView.as_view()),
-    path('patient/<str:pk>/', PatientView.as_view()),
+    path('api/v1/auth/register/', RegistrationView.as_view()),
+    path('api/v1/auth/login/', obtain_auth_token),
+    path('api/v1/patient/', PatientView.as_view()),
+    path('api/v1/patient/<str:pk>/', PatientView.as_view()),
 ]
